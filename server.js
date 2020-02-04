21eaf999c2ce078c3a88d502426283358bd8961f
JG <noreply@jg.com>
2020-02-03 21:03:39 -0800

    added mongo_db URI for heroku deployment

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
 
 
 
